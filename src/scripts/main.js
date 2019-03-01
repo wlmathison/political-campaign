708990b297f5fc510acb80ea35666230f2da2713
Billy Mathison <wlmathison@gmail.com>
2019-03-01 13:44:56 -0600

    input candidate object, completed challenge

---
+++ src/scripts/main.js
@@ -0,0 +1,30 @@
+const candidate = {
+    district: "5th Congressional District",
+    platform: {
+        taxes: "",
+        jobs: "",
+        infrastructure: "",
+        healthCare: "",
+        crimeEnforcement: "",
+    } ,
+    donationURL: "",
+    enventCalendar: "",
+    volunteerInfo: {
+        name:"",
+        address: "",
+        email: "",
+        phoneNumber: "",
+        availability :"",
+        willingActivities: []
+    },
+    biography: "",
+    imageGallery: {
+        headShot: "",
+        familyPic:"",
+        constituentPic: "",
+    },
+    missionStatement: "",
+    voterRegistrationURL: "",
+};
+
+console.table(candidate);
